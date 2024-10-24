d8b5870314859b51c158ea888dd5175b105a8d00
salarcong <salarcong@miumg.edu.gt>
2024-10-23 23:55:11 -0600

    Ruda defecto Login

--- client/vite-project/src/App.jsx
+++ client/vite-project/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Login from './auth/Login';
 import Register from './auth/Register';
 import AdminDashboard from './pages/AdminDashboard';
@@ -9,6 +9,7 @@ const App = () => {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/admin-dashboard" element={<AdminDashboard />} />
